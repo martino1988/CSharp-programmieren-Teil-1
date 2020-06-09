34dc2abc84900e972d9afe272fd9deaee77d35f4
Martin Weltler <1810640050@fh-burgenland.at>
2020-06-09 19:13:36 +0200

    Commit

--- 011 Benzinverbrauch/Program.cs
+++ 011 Benzinverbrauch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace _011_Benzinverbrauch
 {
@@ -6,7 +7,20 @@ namespace _011_Benzinverbrauch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            //Geben Sie über Konsole die Anzahl der gefahrenen Kilometer und die verbrauchten Liter Benzin ein.
+            Console.WriteLine("Wie viele Kilometer bist du gefahren?");
+            double km = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Wie viele Liter Treibstoff hast du verbraucht?");
+            double liter = Convert.ToDouble(Console.ReadLine());
+
+            //Berechnen Sie den Benzinverbrauch pro 100 km und 
+            double verbr100km = (liter / km) * 100;
+
+            //das für die Strecke angefallene Amtliche Kilometergeld(0.42€/ km).
+            double kmgeld = km * 0.42;
+
+            Console.WriteLine("Durchschn. Verbrauch für 100km: " + verbr100km + " Liter\nKilometergeld: "  + kmgeld + " €");
         }
     }
 }
