bf52d9f43055a79af0f4cfc5a164e85c7fd18775
Martin Weltler <1810640050@fh-burgenland.at>
2020-12-30 22:55:50 +0100

    029 und 030

---
+++ 029 Collatz Folge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _029_Collatz_Folge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Collatz Folge 
+            //Eine Collatz-Folge für den Wert n ist definiert durch 
+            //    n -> n / 2, falls n gerade ist, n -> 3n + 1, falls n ungerade ist. 
+            //    Die Folge ist beendet, wenn 1 erreicht ist. 
+            //    Beispiel für n = 7: 7-> 22-> 11-> 34-> 17-> 52-> 26-> 13-> 40-> 20-> 10-> 5-> 16-> 8-> 4-> 2-> 1 
+            //    Implementieren Sie ein Programm zur Berechnung der Collatzfolge für eine Zahl, die über Konsole eingegeben wird.       
+
+            Console.WriteLine("Collatz Zahl eingeben: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while(n >= 2)
+            {
+                Console.Write(n + " ");
+                if(n % 2 == 0) //Wenn n gerade
+                {
+                    n = n / 2;
+                }
+                else if(n % 2 != 0) //Wenn n ungerade
+                {
+                    n = 3 * n + 1;
+                }
+            }
+            Console.WriteLine("--> " + n);
+        }
+    }
+}
---
+++ 030 Bierverkäufer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _030_Bierverkäufer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bierverkäufer Bei Start des Programmes haben Sie 99 Flaschen Bier im Kühlschrank. 
+            //Über Konsole wird eingegeben, wie viele Bierflaschen Ihr Kunde kaufen will. 
+            //Das geht solange bis Sie den gesamten Biervorrat verkauft haben.        
+
+            int bestand = 99;
+
+            while(bestand > 0)
+            {
+                Console.WriteLine("Wie viele Flaschen möchten Sie kaufen?");
+                int bestellung = Convert.ToInt32(Console.ReadLine());
+
+                if(bestellung <= bestand)
+                {
+                    bestand -= bestellung;
+                    Console.WriteLine("Bestellung abgeschlossen," + bestand + " Flaschen im Lager");
+                }
+                else
+                {
+                    Console.WriteLine("Bestellung nicht möglich! Bestellung ist " + bestellung + " und im Lager befinden sich nur mehr " + bestand +" Flaschen.\nBitte Bestellung anpassen!");
+                }
+
+            }
+            Console.WriteLine("Bestand aufgebraucht: " + bestand);
+
+        }
+    }
+}
