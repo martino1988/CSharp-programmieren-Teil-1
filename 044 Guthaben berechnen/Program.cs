a08f327a5495922dacd3e65d6caa76dc43ec1bf1
Martin Weltler <1810640050@fh-burgenland.at>
2021-01-05 15:20:46 +0100

    methoden 1

---
+++ 044 Guthaben berechnen/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _044_Guthaben_berechnen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implementieren Sie die Methode double EndkapitalBerechnen(int guthaben, int laufzeit, double zinssatz). 
+            //Diese Methode berechnet den Wert des guthaben nach Ablauf von laufzeit Jahren unter Anwendung von zinssatz 
+            //mit Berücksichtigung von Zinseszinsen und gibt das Resultat zurück.
+
+            while (true)
+            {
+                Console.WriteLine("Guthaben eingeben (ctrl+c zum beenden):");
+                int g = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Laufzeit (in Jahren) eingeben:");
+                int l = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Zinssatz eingeben (zB 4,25 für 4,25%):");
+                double z = Convert.ToDouble(Console.ReadLine());
+
+                double erg = Endkapitalberechnen(g, l, z);
+                Console.WriteLine("Endkapital: " + erg);
+            }
+
+
+
+
+        }
+
+        public static double Endkapitalberechnen(int guthaben, int laufzeit, double zinssatz)
+        {
+            double TempPSatz = 1 + (zinssatz / 100);
+            double ErgPSatz = Math.Pow(TempPSatz, laufzeit);
+            double Endkapital = guthaben * ErgPSatz;
+            return Endkapital;
+        }
+    }
+}
---
+++ 045 Umfang und Fläche berechnen/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _045_Umfang_und_Fläche_berechnen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implementieren Sie zwei Methoden zur Berechnung von Umfang und Fläche eines Rechtecks bzw Quadrats.
+            //1. int Umfang(int seiteA, int seiteB)
+            //2. int Flaeche(int seiteA, int seiteB)
+            while (true)
+            {
+                Console.WriteLine("Seite A eingeben:");
+                double sa = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Seite B eingeben:");
+                double sb = Convert.ToDouble(Console.ReadLine());
+
+                double u = UmfangBerechnen(sa, sb);
+                double f = Flaecheberechnen(sa, sb);
+                Console.WriteLine("Umfang: " + u + " Flaeche: " + f);
+            }
+        }
+
+        public static double UmfangBerechnen(double seiteA, double seiteB)
+        {
+            return ((2*seiteA)+(2*seiteB));
+        }
+        public static double Flaecheberechnen(double seiteA, double seiteB)
+        {
+            return (seiteA*seiteB);
+        }
+    }
+}
